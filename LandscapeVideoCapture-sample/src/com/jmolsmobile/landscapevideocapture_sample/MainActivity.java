709adf6ef4240e93309b8b59418b4abc6c02a1b2
Jeroen Mols <jmols.mobile@gmail.com>
2014-11-20 18:50:09 +0100

    Added basic launch of VideoCaptureActivity

--- LandscapeVideoCapture-sample/src/com/jmolsmobile/landscapevideocapture_sample/MainActivity.java
+++ LandscapeVideoCapture-sample/src/com/jmolsmobile/landscapevideocapture_sample/MainActivity.java
@@ -1,11 +1,16 @@
 package com.jmolsmobile.landscapevideocapture_sample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.jmolsmobile.landscapevideocapture.VideoCaptureActivity;
 
 public class MainActivity extends FragmentActivity {
 
@@ -14,8 +19,7 @@ public class MainActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+			getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
 
@@ -28,10 +32,17 @@ public class MainActivity extends FragmentActivity {
 		}
 
 		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+			final Button captureBtn = (Button) rootView.findViewById(R.id.btn_capturevideo);
+			captureBtn.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					final Intent intent = new Intent(getActivity(), VideoCaptureActivity.class);
+					startActivityForResult(intent, 101);
+				}
+			});
+
 			return rootView;
 		}
 	}
