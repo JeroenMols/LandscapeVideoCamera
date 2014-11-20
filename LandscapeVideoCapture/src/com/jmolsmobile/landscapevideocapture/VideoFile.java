b609deaf26e505293671daf5468e69a585cfb635
Jeroen Mols <jmols.mobile@gmail.com>
2014-11-20 21:09:13 +0100

    Videofile create object

---
+++ LandscapeVideoCapture/src/com/jmolsmobile/landscapevideocapture/VideoFile.java
@@ -0,0 +1,9 @@
+package com.jmolsmobile.landscapevideocapture;
+
+public class VideoFile {
+
+	public VideoFile(String filename) {
+
+	}
+
+}
---
+++ LandscapeVideoCaptureTest/src/com/jmolsmobile/landscapevideocapture/test/VideoFileTest.java
@@ -0,0 +1,13 @@
+package com.jmolsmobile.landscapevideocapture.test;
+
+import junit.framework.TestCase;
+
+import com.jmolsmobile.landscapevideocapture.VideoFile;
+
+public class VideoFileTest extends TestCase {
+
+	public void test_canCreateObject() {
+		new VideoFile("");
+	}
+
+}
