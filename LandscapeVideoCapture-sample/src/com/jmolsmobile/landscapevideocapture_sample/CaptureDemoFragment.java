99299a3750a02dec4e3b997597cb1aa08cdb3521
Jeroen Mols <jmols.mobile@gmail.com>
2014-11-20 19:14:47 +0100

    Added capture status message

--- LandscapeVideoCapture-sample/src/com/jmolsmobile/landscapevideocapture_sample/CaptureDemoFragment.java
+++ LandscapeVideoCapture-sample/src/com/jmolsmobile/landscapevideocapture_sample/CaptureDemoFragment.java
@@ -1,5 +1,6 @@
 package com.jmolsmobile.landscapevideocapture_sample;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.jmolsmobile.landscapevideocapture.VideoCaptureActivity;
 
@@ -15,6 +17,10 @@ import com.jmolsmobile.landscapevideocapture.VideoCaptureActivity;
  * A placeholder fragment containing a simple view.
  */
 public class CaptureDemoFragment extends Fragment {
+
+	private final String	KEY_STATUSMESSAGE	= "com.jmolsmobile.statusmessage";
+
+	private TextView		statusTv;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +34,31 @@ public class CaptureDemoFragment extends Fragment {
 			}
 		});
 
+		statusTv = (TextView) rootView.findViewById(R.id.tv_status);
+		if (savedInstanceState != null) {
+			statusTv.setText(savedInstanceState.getString(KEY_STATUSMESSAGE));
+		}
+
 		return rootView;
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		outState.putString(KEY_STATUSMESSAGE, statusTv.getText().toString());
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		if (resultCode == Activity.RESULT_OK) {
+			statusTv.setText(R.string.status_capturesuccess);
+		} else if (resultCode == Activity.RESULT_CANCELED) {
+			statusTv.setText(R.string.status_capturecancelled);
+		} else if (resultCode == VideoCaptureActivity.RESULT_ERROR) {
+			statusTv.setText(R.string.status_capturefailed);
+		}
+
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 }
