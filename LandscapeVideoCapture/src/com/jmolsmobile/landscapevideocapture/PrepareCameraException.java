79cfbbff8e119e39307b41a0e7c9b3086a29c84b
Jeroen Mols <jmols.mobile@gmail.com>
2014-11-30 14:14:55 +0100

    Added logging to exception

--- LandscapeVideoCapture/src/com/jmolsmobile/landscapevideocapture/PrepareCameraException.java
+++ LandscapeVideoCapture/src/com/jmolsmobile/landscapevideocapture/PrepareCameraException.java
@@ -5,10 +5,14 @@ package com.jmolsmobile.landscapevideocapture;
  */
 public class PrepareCameraException extends Exception {
 
-	private static final String	MESSAGE	= "Unable to use camera for recording";
+	private static final String	LOG_PREFIX			= "Unable to unlock camera - ";
+	private static final String	MESSAGE				= "Unable to use camera for recording";
+
+	private static final long	serialVersionUID	= 6305923762266448674L;
 
 	@Override
 	public String getMessage() {
+		CLog.e(CLog.EXCEPTION, LOG_PREFIX + MESSAGE);
 		return MESSAGE;
 	}
 }
