0e5b8da210dad4a6276fff65b1123940294a82cb
Jeroen Mols <jmols.mobile@gmail.com>
2014-12-01 16:48:26 +0100

    Removed unused methods from log class

--- LandscapeVideoCapture/src/com/jmolsmobile/landscapevideocapture/CLog.java
+++ LandscapeVideoCapture/src/com/jmolsmobile/landscapevideocapture/CLog.java
@@ -13,16 +13,8 @@ public class CLog {
 	public static final String	HELPER		= "VideoCapture_CaptureHelper";
 	public static final String	RECORDER	= "VideoCapture_VideoRecorder";
 
-	public static void v(String tag, String msg) {
-		Log.v(tag, msg);
-	}
-
 	public static void d(String tag, String msg) {
 		Log.d(tag, msg);
-	}
-
-	public static void i(String tag, String msg) {
-		Log.i(tag, msg);
 	}
 
 	public static void e(String tag, String msg) {
