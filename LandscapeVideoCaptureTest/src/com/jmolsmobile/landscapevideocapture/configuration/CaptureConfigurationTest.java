c33a423cddd01d99343aa65ce602200b20bfa480
Jeroen Mols <jeroen.mols.be@gmail.com>
2015-08-12 08:36:40 +0200

    Fixed failing unit tests due to changes in captureconfiguration

--- LandscapeVideoCaptureTest/src/com/jmolsmobile/landscapevideocapture/configuration/CaptureConfigurationTest.java
+++ LandscapeVideoCaptureTest/src/com/jmolsmobile/landscapevideocapture/configuration/CaptureConfigurationTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014 Jeroen Mols
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,49 +23,49 @@ import junit.framework.TestCase;
 
 public class CaptureConfigurationTest extends TestCase {
 
-	public void test_defaultConfiguration() throws Exception {
-		final CaptureConfiguration config = new CaptureConfiguration();
+    public void test_defaultConfiguration() throws Exception {
+        final CaptureConfiguration config = new CaptureConfiguration();
 
-		checkConfiguration(config, CaptureResolution.RES_720P.width, CaptureResolution.RES_720P.height,
-				CaptureResolution.RES_720P.getBitrate(CaptureQuality.HIGH), CaptureConfiguration.NO_DURATION_LIMIT,
-				CaptureConfiguration.NO_FILESIZE_LIMIT);
-	}
+        checkConfiguration(config, CaptureResolution.RES_720P.width, CaptureResolution.RES_720P.height,
+                CaptureResolution.RES_720P.getBitrate(CaptureQuality.HIGH), CaptureConfiguration.NO_DURATION_LIMIT,
+                CaptureConfiguration.NO_FILESIZE_LIMIT);
+    }
 
-	public void test_predefinedConfiguration() throws Exception {
-		final CaptureConfiguration config = new CaptureConfiguration(CaptureResolution.RES_2160P, CaptureQuality.LOW);
+    public void test_predefinedConfiguration() throws Exception {
+        final CaptureConfiguration config = new CaptureConfiguration(CaptureResolution.RES_2160P, CaptureQuality.LOW);
 
-		checkConfiguration(config, CaptureResolution.RES_2160P.width, CaptureResolution.RES_2160P.height,
-				CaptureResolution.RES_2160P.getBitrate(CaptureQuality.LOW), CaptureConfiguration.NO_DURATION_LIMIT,
-				CaptureConfiguration.NO_FILESIZE_LIMIT);
-	}
+        checkConfiguration(config, CaptureResolution.RES_2160P.width, CaptureResolution.RES_2160P.height,
+                CaptureResolution.RES_2160P.getBitrate(CaptureQuality.LOW), CaptureConfiguration.NO_DURATION_LIMIT,
+                CaptureConfiguration.NO_FILESIZE_LIMIT);
+    }
 
-	public void test_predefinedConfigurationWithSizeDuration() throws Exception {
-		final CaptureConfiguration config = new CaptureConfiguration(CaptureResolution.RES_1080P,
-				CaptureQuality.MEDIUM, 50, 10);
+    public void test_predefinedConfigurationWithSizeDuration() throws Exception {
+        final CaptureConfiguration config = new CaptureConfiguration(CaptureResolution.RES_1080P,
+                CaptureQuality.MEDIUM, 50, 10);
 
-		checkConfiguration(config, CaptureResolution.RES_1080P.width, CaptureResolution.RES_1080P.height,
-				CaptureResolution.RES_1080P.getBitrate(CaptureQuality.MEDIUM), 50 * 1000, 10 * 1024 * 1024);
-	}
+        checkConfiguration(config, CaptureResolution.RES_1080P.width, CaptureResolution.RES_1080P.height,
+                CaptureResolution.RES_1080P.getBitrate(CaptureQuality.MEDIUM), 50, 10 * 1024 * 1024);
+    }
 
-	public void test_configurationWithHeightWidthBitrate() throws Exception {
-		final CaptureConfiguration config = new CaptureConfiguration(200, 300, 5000000);
+    public void test_configurationWithHeightWidthBitrate() throws Exception {
+        final CaptureConfiguration config = new CaptureConfiguration(200, 300, 5000000);
 
-		checkConfiguration(config, 200, 300, 5000000, CaptureConfiguration.NO_DURATION_LIMIT,
-				CaptureConfiguration.NO_FILESIZE_LIMIT);
-	}
+        checkConfiguration(config, 200, 300, 5000000, CaptureConfiguration.NO_DURATION_LIMIT,
+                CaptureConfiguration.NO_FILESIZE_LIMIT);
+    }
 
-	public void test_configurationWithHeightWidthBitrateSizeDuration() throws Exception {
-		final CaptureConfiguration config = new CaptureConfiguration(200, 300, 5000000, 1, 100);
+    public void test_configurationWithHeightWidthBitrateSizeDuration() throws Exception {
+        final CaptureConfiguration config = new CaptureConfiguration(200, 300, 5000000, 1, 100);
 
-		checkConfiguration(config, 200, 300, 5000000, 1 * 1000, 100 * 1024 * 1024);
-	}
+        checkConfiguration(config, 200, 300, 5000000, 1, 100 * 1024 * 1024);
+    }
 
-	private void checkConfiguration(final CaptureConfiguration config, int width, int height, int bitrate,
-			int duration, int filesize) {
-		assertEquals(config.getVideoWidth(), width);
-		assertEquals(config.getVideoHeight(), height);
-		assertEquals(config.getVideoBitrate(), bitrate);
-		assertEquals(config.getMaxCaptureDuration(), duration);
-		assertEquals(config.getMaxCaptureFileSize(), filesize);
-	}
+    private void checkConfiguration(final CaptureConfiguration config, int width, int height, int bitrate,
+                                    int duration, int filesize) {
+        assertEquals(config.getVideoWidth(), width);
+        assertEquals(config.getVideoHeight(), height);
+        assertEquals(config.getVideoBitrate(), bitrate);
+        assertEquals(config.getMaxCaptureDuration(), duration);
+        assertEquals(config.getMaxCaptureFileSize(), filesize);
+    }
 }
