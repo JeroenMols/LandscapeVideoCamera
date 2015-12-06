fb6d1095dcf45edc58f7a368bd4ae6975dd2d060
Jeroen Mols <jeroen.mols.be@gmail.com>
2015-12-06 19:47:16 +0100

    Return width and height

--- library/src/androidTest/java/com/jmolsmobile/landscapevideocapture/camera/CameraSizeTest.java
+++ library/src/androidTest/java/com/jmolsmobile/landscapevideocapture/camera/CameraSizeTest.java
@@ -6,6 +6,8 @@ import android.test.suitebuilder.annotation.SmallTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by Jeroen Mols on 06/12/15.
  */
@@ -18,4 +20,17 @@ public class CameraSizeTest {
         new CameraSize(0, 0);
     }
 
+    @Test
+    public void returnWidth() throws Exception {
+        CameraSize cameraSize = new CameraSize(800, 600);
+
+        assertEquals(800, cameraSize.getWidth());
+    }
+
+    @Test
+    public void returnHeight() throws Exception {
+        CameraSize cameraSize = new CameraSize(800, 600);
+
+        assertEquals(600, cameraSize.getHeight());
+    }
 }
--- library/src/main/java/com/jmolsmobile/landscapevideocapture/camera/CameraSize.java
+++ library/src/main/java/com/jmolsmobile/landscapevideocapture/camera/CameraSize.java
@@ -4,6 +4,20 @@ package com.jmolsmobile.landscapevideocapture.camera;
  * Created by Jeroen Mols on 06/12/15.
  */
 public class CameraSize {
+
+    private final int width;
+    private final int height;
+
     public CameraSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 }
