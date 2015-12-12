1f0ccc53cb4378d85ee5174aad65cb3201713fdd
Jeroen Mols <jeroen.mols.be@gmail.com>
2015-12-12 22:23:29 +0100

    Added CLog test class

---
+++ library/src/androidTest/java/com/jmolsmobile/landscapevideocapture/ClogTest.java
@@ -0,0 +1,20 @@
+package com.jmolsmobile.landscapevideocapture;
+
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Jeroen Mols on 12/12/15.
+ */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class ClogTest extends CLog {
+
+    @Test
+    public void emptyTest() throws Exception {
+
+    }
+}
